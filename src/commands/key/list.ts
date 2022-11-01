a3ff2decc7406ad3a13404719ddfbb0df1c28ee1
the-masthead <116779271+the-masthead@users.noreply.github.com>
2022-11-01 15:51:20 -0700

    Add key list command

---
+++ src/commands/key/list.ts
@@ -0,0 +1,16 @@
+import { Command } from "@oclif/core";
+import { listWallets } from "../../keystore";
+
+export default class KeyList extends Command {
+  static description = "Lists keys saved in the encrypted keystore.";
+  static examples = ["<%= config.bin %> <%= command.id %>"];
+  static usage = "key list";
+
+  public async run(): Promise<void> {
+    await this.parse(KeyList);
+    const keys = await listWallets();
+    for (let i = 0; i < keys.length; ++i) {
+      console.log(keys[i]);
+    }
+  }
+}
