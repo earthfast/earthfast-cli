69dfa35f2d82b2516e2637528696ca29c91dd15a
the-masthead <116779271+the-masthead@users.noreply.github.com>
2022-11-01 18:02:17 -0700

    Support project unpublishing

--- src/commands/project/content.ts
+++ src/commands/project/content.ts
@@ -5,17 +5,24 @@ import { decodeEvent, getContract, getSigner, normalizeHex } from "../../helpers
 
 export default class ProjectContent extends TransactionCommand {
   static description = "Publishes the provided bundle on the network.";
-  static examples = ["<%= config.bin %> <%= command.id %> 0x123abc... https://.../my-site-v1.0.0.tgz 0x456def..."];
+  static examples = [
+    "<%= config.bin %> <%= command.id %> 0x123abc... https://.../my-site-v1.0.0.tgz 0x456def...",
+    "<%= config.bin %> <%= command.id %> 0x123abc... '' '' # Unpublishes the project content",
+  ];
   static usage = "<%= command.id %> ID URL SHA";
   static aliases = ["project:content", "project:publish"];
   static args: Arg[] = [
     { name: "ID", description: "The ID of the project to publish to.", required: true },
-    { name: "URL", description: "The public URL to fetch the bundle.", required: true },
-    { name: "SHA", description: "The SHA-256 checksum of the bundle.", required: true },
+    { name: "URL", description: "The public URL to fetch the bundle." },
+    { name: "SHA", description: "The SHA-256 checksum of the bundle." },
   ];
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(ProjectContent);
+    if (args.URL === undefined || args.SHA === undefined) {
+      this.error("URL and SHA must be specified");
+    }
+
     const signer = await getSigner(flags.network, flags.ledger);
     const projects = await getContract(flags.network, "projects", signer);
     const projectId = normalizeHex(args.ID);
