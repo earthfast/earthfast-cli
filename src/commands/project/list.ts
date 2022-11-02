74d8251df6bee3ce31466acdb14245817de5dc13
the-masthead <116779271+the-masthead@users.noreply.github.com>
2022-11-01 17:27:52 -0700

    Add project list command

---
+++ src/commands/project/list.ts
@@ -0,0 +1,26 @@
+import { Flags } from "@oclif/core";
+import { BlockchainCommand } from "../../base";
+import { getContract, getProvider, normalizeHex } from "../../helpers";
+
+export default class ProjectList extends BlockchainCommand {
+  static description = "Lists projects on the Armada Network.";
+  static examples = ["<%= config.bin %> <%= command.id %>"];
+  static usage = "<%= command.id %> [--owner ADDR] [--skip N] [--size N]";
+  static flags = {
+    owner: Flags.string({ description: "Filter by owner address.", helpValue: "ADDR" }),
+    skip: Flags.integer({ description: "The number of results to skip.", helpValue: "N", default: 0 }),
+    size: Flags.integer({ description: "The number of results to list.", helpValue: "N", default: 100 }),
+  };
+
+  public async run(): Promise<void> {
+    const { flags } = await this.parse(ProjectList);
+    const provider = await getProvider(flags.network);
+    const projects = await getContract(flags.network, "projects", provider);
+    let data = await projects.getProjects(flags.skip, flags.size);
+    if (flags.owner) {
+      const owner = normalizeHex(flags.owner);
+      data = data.filter((value: { owner: string }) => value.owner.toLowerCase() === owner.toLowerCase());
+    }
+    console.log(data);
+  }
+}
