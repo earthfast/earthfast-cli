5d7e4ae462c93611c5fcb2adcf94804c48a61cd5
the-masthead <116779271+the-masthead@users.noreply.github.com>
2022-11-03 12:48:25 -0700

    Add escrow deposit command

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,14 +2,16 @@ import fs from "fs";
 import type { ContractInterface } from "ethers";
 
 // These imports are necessary to pull these files into dist/
+import "../abi/staging/ArmadaToken.json";
 import "../abi/staging/ArmadaNodes.json";
 import "../abi/staging/ArmadaProjects.json";
 import "../abi/staging/ArmadaReservations.json";
+import "../abi/testnet/ArmadaToken.json";
 import "../abi/testnet/ArmadaNodes.json";
 import "../abi/testnet/ArmadaProjects.json";
 import "../abi/testnet/ArmadaReservations.json";
 
-export type ContractName = "ArmadaNodes" | "ArmadaProjects" | "ArmadaReservations";
+export type ContractName = "ArmadaToken" | "ArmadaNodes" | "ArmadaProjects" | "ArmadaReservations";
 
 export interface ContractInfo {
   address: string;
