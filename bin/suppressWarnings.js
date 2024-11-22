ab2c47a6059b8a1f2a131bc8f06e8222c7dfbeb9
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2024-11-22 11:06:45 -0500

    Alternate bin approach

--- bin/suppressWarnings.js
+++
@@ -1,6 +0,0 @@
-// Suppress all process warnings
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-process.emitWarning = () => {};
-// Set other warning-related configurations
-process.env.NODE_NO_WARNINGS = "1";
-process.env.NODE_NO_TIMEOUT_WARNING = "1";
