c4a82d057ac90f9740a20eaef9eddf0e49b006eb
Jorge Monroy <jlmonroy13@gmail.com>
2024-11-27 08:14:06 -0500

    Add reservations flag to project show command

--- src/commands/project/show.ts
+++ src/commands/project/show.ts
@@ -1,20 +1,32 @@
+import { Flags } from "@oclif/core";
 import { Arg } from "@oclif/core/lib/interfaces";
+import { Result } from "ethers/lib/utils";
 import { BlockchainCommand } from "../../base";
-import { formatProject, getContract, getProvider, parseHash, pretty } from "../../helpers";
+import { formatNode, formatProject, getContract, getProvider, parseHash, pretty } from "../../helpers";
 
 export default class ProjectShow extends BlockchainCommand {
   static summary = "Show details of an EarthFast Network project.";
   static examples = ["<%= config.bin %> <%= command.id %> 0x123abc..."];
   static usage = "<%= command.id %> ID";
   static args: Arg[] = [{ name: "ID", description: "The ID of the project to show.", required: true }];
+  static flags = {
+    reservations: Flags.boolean({ description: "Show reservations for the project", default: false }),
+  };
 
   public async run(): Promise<Record<string, unknown>> {
     const { args, flags } = await this.parse(ProjectShow);
     const provider = await getProvider(flags.network, flags.rpc);
     const projects = await getContract(flags.network, flags.abi, "EarthfastProjects", provider);
+    const reservations = await getContract(flags.network, flags.abi, "EarthfastReservations", provider);
     const projectId = parseHash(args.ID);
     const record = await projects.getProject(projectId);
     const output = formatProject(record);
+
+    if (flags.reservations) {
+      const reservationResults: Result[] = await reservations.getReservations(projectId, 0, 1000);
+      output.reservations = reservationResults.map((r) => formatNode(r));
+    }
+
     this.log(pretty(output));
     return output;
   }
